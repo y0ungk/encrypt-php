f20ef7897f1438e540d14b5889676de8f60ad726
Young Kim <young@get.it>
2014-10-06 17:38:27 -0700

    Used as instance and not statically anymore

--- tests/y0ungk/Encryption/Aes256Test.php
+++ tests/y0ungk/Encryption/Aes256Test.php
@@ -8,9 +8,10 @@ class Aes256Test extends \PHPUnit_Framework_TestCase {
     public function testEncryption() {
         $aesKey = openssl_random_pseudo_bytes(Aes256::KEY_SIZE);
         $hmacKey = openssl_random_pseudo_bytes(Aes256::KEY_SIZE);
-        $message = 'hello world';
-        $encryptedMessage = Aes256::encrypt($message, $aesKey, $hmacKey);
-        $decryptedMessage = Aes256::decrypt($encryptedMessage, $aesKey, $hmacKey);
-        $this->assertEquals($message, $decryptedMessage);
+        $plainText = 'hello world';
+        $encryption = new Aes256($aesKey, $hmacKey);
+        $encryptedText = $encryption->encrypt($plainText);
+        $decryptedText = $encryption->decrypt($encryptedText);
+        $this->assertEquals($plainText, $decryptedText);
     }
 }
