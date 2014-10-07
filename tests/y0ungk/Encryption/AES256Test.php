5bb64f4975f718728cbfe4dbfb1d659cd6bbef09
Young Kim <young@get.it>
2014-10-06 17:28:27 -0700

    fix issue from using namespaces

--- tests/y0ungk/Encryption/AES256Test.php
+++ tests/y0ungk/Encryption/AES256Test.php
@@ -1,12 +1,16 @@
 <?php
 
-class AES256Test extends PHPUnit_Framework_TestCase {
+namespace y0ungk\Encryption\Tests;
+
+use y0ungk\Encryption\Aes256;
+
+class Aes256Test extends \PHPUnit_Framework_TestCase {
     public function testEncryption() {
-        $aesKey = openssl_random_pseudo_bytes(AES256::KEY_SIZE);
-        $hmacKey = openssl_random_pseudo_bytes(AES256::KEY_SIZE);
+        $aesKey = openssl_random_pseudo_bytes(Aes256::KEY_SIZE);
+        $hmacKey = openssl_random_pseudo_bytes(Aes256::KEY_SIZE);
         $message = 'hello world';
-        $encryptedMessage = AES256::encrypt($message, $aesKey, $hmacKey);
-        $decryptedMessage = AES256::decrypt($encryptedMessage, $aesKey, $hmacKey);
+        $encryptedMessage = Aes256::encrypt($message, $aesKey, $hmacKey);
+        $decryptedMessage = Aes256::decrypt($encryptedMessage, $aesKey, $hmacKey);
         $this->assertEquals($message, $decryptedMessage);
     }
 }
